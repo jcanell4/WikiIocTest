0494c1135ec6bf08e2eb451ded4be3afea80d8a2
jcanell4 <jcanell4@ioc.cat>
2014-10-07 00:28:34 +0200

    Update DokuModelAdapterTestCase.php
    Delete the first instruction to kill the execution if DOKU_INC is undefined and rename the attribute named instance to adapter

--- testLib/DokuModelAdapterTestCase.php
+++ testLib/DokuModelAdapterTestCase.php
@@ -1,34 +1,26 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of DokuModelAdapterTestCase
  *
  * @author Daniel Criado Casas<dani.criado.casas@gmail.com>
  */
-if (!defined('DOKU_INC'))
-    die();
 require_once 'DokuwikiTestCase.php';
 require_once (DOKU_INC.'lib/plugins/wikiiocmodel/DokuModelAdapter.php');
 class DokuModelAdapterTestCase extends DokuwikiTestCase{
-    private $instance;
+    private $adapter;
     
     /**
      * Constructor
      */
     function DokuModelAdapterTestCase() {
-        $this->instance = new DokuModelAdapter();
+        $this->adapter = new DokuModelAdapter();
     }
     
     /**
      * Getter de la instància
      */
     function getDokuModelAdapter(){
-        return $this->instance;
+        return $this->adapter;
     }
 }
