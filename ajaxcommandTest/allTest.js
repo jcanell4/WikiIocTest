f1afe2aaf7fe83aa0b94b97ddbb8a8245dd021b0
Dani Criado <dani.criado.casas@gmail.com>
2014-11-13 11:04:03 +0100

    nou ajaxcommand test: new_pageTest, saveTest nou jsTest: DataTypeChecker

--- ajaxcommandTest/allTest.js
+++ ajaxcommandTest/allTest.js
@@ -4,5 +4,8 @@ define(["dojo/has", "doh/main", "require"], function(has, doh, require){
         doh.register("auth_commandreportTest", require.toUrl("./auth_commandreportTest.html"), 25000);
         doh.register("loginTest", require.toUrl("./loginTest.html"), 25000);
         doh.register("pageTest", require.toUrl("./pageTest.html"), 25000);
+        
+        doh.register("new_pageTest", require.toUrl("./new_pageTest.html"), 25000);
+        doh.register("saveTest", require.toUrl("./saveTest.html"), 25000);
     }
 });
---
+++ jsTestLib/DataTypeChecker.js
@@ -0,0 +1,50 @@
+define([
+    "dojo/_base/declare"
+   ,"test/ObjectChecker"
+   ,"test/AttRegexChecker"
+   ,"test/AttObjectChecker"
+   ,"test/HasAttributeChecker"
+   ,"dojo/_base/lang"   
+], function(declare, ObjectChecker, AttRegexChecker, AttObjectChecker, 
+                HasAttributeChecker, lang){
+    var ret = declare("test.DataTypeChecker", [ObjectChecker], {
+        id:null
+        ,"-chains-": {
+            constructor: "manual"  //evita la crida al constructor del pare
+        }
+        ,constructor: function(att){ 
+            if(lang.isObject(att)){ //Se li pot pasar un objecte amb els mateixos noms d'atributs per fer una "fusió"
+                lang.mixin(this, att);
+            }else{
+                if(att){
+                    this.id = att;
+                }
+            }            
+            var self=this;
+            this.checkers = [
+                new AttRegexChecker({
+                    attributeName:"type",
+                    regex:/data/i}),
+                new AttObjectChecker({
+                    attributeName:"value",
+                    checker: new ObjectChecker({
+                        checkers:[
+                            new AttRegexChecker({
+                                attributeName:"id",
+                                regex:new RegExp(self.id.replace(/:/g, "_"))}),
+                            new AttRegexChecker({
+                                attributeName:"ns",
+                                regex:new RegExp(self.id)}),
+                            new HasAttributeChecker({
+                                attributeName:"title"}),
+                            new HasAttributeChecker({
+                                attributeName:"content"})
+                        ]
+                    })
+                    
+                })
+            ];
+        }
+    });
+    return ret;
+});
