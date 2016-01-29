34b2096d9fd5cc4b7659df85f8bd1054e99e62f2
jcanell4 <jcanell4@xtec.cat>
2016-01-29 01:54:13 +0100

    adaptar el test

--- testLib/DokuwikiTestCase.php
+++ testLib/DokuwikiTestCase.php
@@ -7,7 +7,7 @@
  */
 
 if(!defined('DOKU_INC')) define('DOKU_INC', dirname(__FILE__)."/".DOKU_SRC);
-require_once (DOKU_INC.'inc/init.php');
+require_once DOKU_INC.'inc/init.php';
 class DokuwikiTestCase extends PHPUnit_Framework_TestCase {
     public function __construct($name="DokuwikiTestCase") {
         parent::__construct($name);
